eca0d38f8cf7ff13d8532d88fe8d38db00431508
Madeline Undis <mundis@smith.edu>
2023-09-15 16:54:50 -0500

    rough working version of query - coalesce, etc not finalized

--- app/services/word.ts
+++ app/services/word.ts
@@ -18,7 +18,7 @@ export const loadWord = async () => {
 
 // end;
 // $$ language plpgsql;
-//figure out what's happening with words seen most recently
+
 export const sampleWord = async () => {
   const result = await db.$queryRaw`
   with latest_asks as (
@@ -29,9 +29,26 @@ export const sampleWord = async () => {
       occurrences
     group by word_id
   ),
+   latest_roots as (
+    select r.id,
+    SUM(extract(
+      epoch 
+      from 
+        (
+          date_trunc('day', now() - la.last_seen)
+        )/ 86400
+    )):: int as root_score
+    from roots r 
+    left join connections c on r.id = c.root_id 
+    inner join latest_asks la on c.word_id = la.word_id
+    group by r.id
+  ),
   result as (
     select
-      w.*,
+      w.id,
+    	w.word,
+    	w.difficulty,
+    	(SUM(lr.root_score)/COUNT(c.word_id))::int as root_score,
       extract(
       epoch 
       from 
@@ -42,16 +59,19 @@ export const sampleWord = async () => {
     from
       words w
       left join latest_asks la on w.id = la.word_id
+    	inner join connections c on w.id = c.word_id
+    	inner join latest_roots lr on lr.id = c.root_id
+    	group by w.id, la.last_seen
   )
   select
     *,
-    weight_from_days_ago(days_ago, difficulty, 1) as weight
+    weight_from_days_ago(days_ago, difficulty, 1, root_score) as weight
   from
     result;
 `;
   return result;
 };
-//need to overwrite
+
 export const logOccurrence = async (wordId: number) => {
   const occurrence = await db.occurrences.create({
     data: {
